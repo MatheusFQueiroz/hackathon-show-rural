0495f3b236a238b05fec6327ff084927a3bb041b
joao-silva09 <joao.e.silva.03@gmail.com>
2025-02-13 19:08:50 -0300

    Add Aviarios table.

--- hackathon-showrural/app/(public)/aviarios/page.tsx
+++ hackathon-showrural/app/(public)/aviarios/page.tsx
@@ -1,4 +1,28 @@
+type Payment = {
+  id: string
+  amount: number
+  status: "pending" | "processing" | "success" | "failed"
+  email: string
+}
+
+export const payments: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "489e1d42",
+    amount: 125,
+    status: "processing",
+    email: "example@gmail.com",
+  }
+]
+
 export default function Aviarios() {
-    return <div>Aviários</div>;
+    return <div>
+      <h1>Aviários</h1>
+    </div>;
   }
   
