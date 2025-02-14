c70eedf9e2f68ac2eccd17990a0ffe41da8539f8
giovaniocan <giovaniocan@gmail.com>
2025-02-14 08:32:32 -0300

    ocorrencias almost done

---
+++ hackathon-showrural/app/(public)/ocorrencias/page.tsx
@@ -0,0 +1,36 @@
+// "use client";
+
+import Filter from "@/components/lotes/Filter";
+import ExpandableTable from "@/components/ocorrencias/ExpandableTable";
+import AddLoteForm from "@/components/lotes/AddLoteForm";
+import { Aviario } from "@/app/uteis/types";
+/*
+const aviarios: Aviario[] = [
+  {
+    id: "1",
+    nomeResponsavel: "João",
+    endereco: "Rua 1",
+    isAtivo: true,
+  },
+  {
+    id: "2",
+    nomeResponsavel: "Giovani",
+    endereco: "Rua 3",
+    isAtivo: false,
+  },
+];*/
+
+export default function Ocorrencias() {
+  return (
+    <div>
+      <div className="w-full flex justify-center pt-2">
+        <div className="my-6 w-4/5 flex flex-col justify-center text-center gap-16">
+            <h1 className="text-4xl">
+                Ocorrências
+            </h1>
+          <ExpandableTable />
+        </div>
+      </div>
+    </div>
+  );
+}
