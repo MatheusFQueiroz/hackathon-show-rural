f6784ef6867a90bba07d9088097b5bbcbb8cafd3
joao-silva09 <joao.e.silva.03@gmail.com>
2025-02-13 22:35:40 -0300

    add: aviario.

--- hackathon-showrural/app/(public)/aviarios/page.tsx
+++ hackathon-showrural/app/(public)/aviarios/page.tsx
@@ -1,28 +1,143 @@
+"use client";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Aviario, columns } from "./columns";
 import { DataTable } from "./data-table";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+type Cliente = {
+  id: string;
+  nome: string;
+};
+
+const clientes: Cliente[] = [
+  {
+    id: "1",
+    nome: "João",
+  },
+  {
+    id: "2",
+    nome: "Giovani",
+  },
+];
 
 async function getData(): Promise<Aviario[]> {
   return [
     {
       id: "1",
       nomeResponsavel: "João",
-      endereco: "Rua 1"
+      endereco: "Rua 1",
     },
     {
       id: "2",
       nomeResponsavel: "Giovani",
-      endereco: "Rua 3"
+      endereco: "Rua 3",
     },
-    // ...
   ];
 }
 
 export default async function Aviarios() {
+  const FormSchema = z.object({
+    cliente: z.string(),
+    situacao: z.string(),
+  });
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      cliente: "",
+      situacao: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+  }
+
   const data = await getData();
   return (
     <div>
-      <h1>Aviários</h1>
-      <DataTable columns={columns} data={data} />
+      <div className="w-full flex justify-center">
+        <div className="w-4/5">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="grid grid-cols-3 gap-8 items-end mb-8"
+            >
+              <FormField
+                control={form.control}
+                name="cliente"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Situação Lote</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Nome do Cliente" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {clientes.map((cliente: Cliente) => (
+                          <SelectItem key={cliente.id} value={cliente.id}>
+                            {cliente.nome}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="situacao"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Situação Lote</FormLabel>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={undefined}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectItem value="ativo">Ativo</SelectItem>
+                        <SelectItem value="inativo">Inativo</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Filtrar</Button>
+            </form>
+          </Form>
+          <DataTable columns={columns} data={data} />
+        </div>
+      </div>
     </div>
   );
 }
